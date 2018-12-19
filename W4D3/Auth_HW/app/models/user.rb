e0b43889bba0a8e0a2a1c13b16a27c6ce9fa6d0e
adam <ayl292@nyu.edu>
2018-12-18 22:22:42 -0500

    Finish W4D3

--- W4D3/Auth_HW/app/models/user.rb
+++ W4D3/Auth_HW/app/models/user.rb
@@ -17,7 +17,31 @@ class User < ApplicationRecord
     validates :password, length: { minimum: 6, allow_nil: true }
     before_validation :ensure_session_token
 
+    def self.find_by_credentials(username, password)
+        user = User.find_by(username)
+        if user && (BCrypt::Password.create(user.password_digest).is_password?(password))
+            return user
+        else
+            nil
+        end
+    end
 
+    def self.generate_session_token 
+        SecureRandom::urlsafe_base64(16)
+    end
 
+    def reset_session_token
+        self.session_token = User.generate_session_token
+        self.save!
+        self.session_token
+    end
 
+    def ensure_session_token
+        self.session_token ||= User.generate_session_token
+    end
+
+    def password=(value)
+        @password = value
+        self.password_digest = BCrypt::Password.create(@assword)
+    end
 end
