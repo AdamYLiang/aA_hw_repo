e8f350343dad825374ef41f5bdc41fdc9687c865
adam <ayl292@nyu.edu>
2018-12-23 11:22:21 -0500

    Tests working for W4D5

--- W4D5/spec/models/user_spec.rb
+++ W4D5/spec/models/user_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let!(:user) { create(:user) }
+  it { should validate_presence_of(:email) }
+  it { should validate_presence_of(:password_digest) }
+  it { should validate_presence_of(:session_token) }
+  it { should validate_length_of(:password).is_at_least(6) }
+
+  describe "uniqueness" do
+    # before :each do
+    #   create(:user)
+    # end
+    it { should validate_uniqueness_of(:session_token) }
+    it { should validate_uniqueness_of(:email) }
+  end
 end
