a51106e3eaad097dc2e9c121da4c42d4c4614d32
adam <ayl292@nyu.edu>
2018-12-25 13:34:56 -0500

    Add comments for controllers

--- W4D5/app/controllers/application_controller.rb
+++ W4D5/app/controllers/application_controller.rb
@@ -1,11 +1,14 @@
 class ApplicationController < ActionController::Base
+    #Helper methods so you can use them throughout class
     helper_method :logged_in?, :current_user
+
     #C urrent user
     #E nsure_logged_in
     #L login!
     #L logged_in?
     #L ogout
 
+    #Find user by session token 
     def current_user
         @current_user ||= User.find_by(session_token: session[:session])
     end
@@ -18,10 +21,12 @@ class ApplicationController < ActionController::Base
         session[:session] = user.reset_session_token!
     end
 
+    #Check if current user exists
     def logged_in?
         !!current_user
     end
 
+    #Reset session token and set others to nil 
     def logout!
         current_user.reset_session_token!
         @current_user = nil 
