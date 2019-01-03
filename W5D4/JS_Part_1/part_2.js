ae0d41e60c4c62f57dbb454ed8953eb18c105f72
adam <ayl292@nyu.edu>
2019-01-02 22:52:21 -0500

    Basic elephant object

--- W5D4/JS_Part_1/part_2.js
+++ W5D4/JS_Part_1/part_2.js
@@ -6,3 +6,28 @@ function titleize(name, callback) {
 titleize(["Mary", "Brian", "Leo"], (names) => {
     names.forEach(name => console.log(name))
 })
+
+//Phase 2
+//Ele1 = new Elephant("Ele", 12, ["singing"])
+function Elephant(name, height, tricks) {
+    this.name = name;
+    this.height = height;
+    this.tricks = tricks;
+}
+
+Elephant.prototype.trumpet = function () {
+    console.log(`${this.name} the elephant goes 'phrRRRRRRRRRRR!!!!!!!'`);
+}
+
+Elephant.prototype.grow = function () {
+    this.height = this.height + 12;
+}
+
+Elephant.prototype.addTrick = function (trick) {
+    this.tricks.push(trick)
+}
+
+Elephant.prototype.play = function () {
+    trickIndex = Math.floor(Math.random() * this.tricks.length);
+    console.log(`${this.name} is ${this.tricks[trickIndex]}!`);
+}
