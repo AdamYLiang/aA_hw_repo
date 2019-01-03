ad3c0b08a5011d5699ef88a67527087f9f0e3a74
adam <ayl292@nyu.edu>
2019-01-02 21:29:44 -0500

    W5D4

---
+++ W5D4/JS_Part_1/part_1.js
@@ -0,0 +1,7 @@
+function madlib(verb, adj, noun) {
+    return "We shall " + verb.toUpperCase() + " make the " + adj.toUpperCase() + " " + noun.toUpperCase();
+}
+
+function isSubstring(searchString, subString) {
+    return searchString.includes(subString);
+}
