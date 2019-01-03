d757a0f7195a7dfceb662046a2e6c06a2421c28f
adam <ayl292@nyu.edu>
2019-01-02 22:06:52 -0500

    Part 1 Complete

--- W5D4/JS_Part_1/part_1.js
+++ W5D4/JS_Part_1/part_1.js
@@ -5,3 +5,42 @@ function madlib(verb, adj, noun) {
 function isSubstring(searchString, subString) {
     return searchString.includes(subString);
 }
+
+function fizzBuzz(array) {
+    var finalArray = [];
+    array.forEach(el => {
+        if ((el % 3 === 0) ^ (el % 5 === 0)) {
+            finalArray.push(el);
+        }
+    })
+
+    return finalArray;
+}
+
+function isPrime(num) {
+    if (num < 2) return false;
+
+    for (var i = 2; i < num; i++) {
+        if (num % i === 0) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+function sumOfNPrimes(n) {
+    var sum = 0;
+    var sumCounter = 0;
+    let i = 2; 
+
+    while (sumCounter <= n) {
+        if (isPrime(i)) {
+            sum += i
+            sumCounter++;
+        }
+        i++;
+    }
+    
+    return sum;
+}
