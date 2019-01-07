dcc0265b6bed5009fb56511de20ea8f3bcc37c16
adam <ayl292@nyu.edu>
2019-01-06 22:18:57 -0500

    Canvas demo

---
+++ W6D1/canvas_practice/practice.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function(){
+    const mycanvas = document.getElementById("mycanvas");
+    mycanvas.width = 500;
+    mycanvas.height = 500;
+
+    const ctx = mycanvan.getContext("2d");
+});
